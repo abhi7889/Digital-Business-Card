fc4190dbdfb506c301d2ebcddc434309355c946f
abhi7889 <abhishek131214@gmail.com>
2023-09-25 21:39:54 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -4,15 +4,14 @@ import "./app.css";
 import Interest from "./Components/Interest";
 import Icons from "./Components/icons";
 
-
 export default function App() {
   return (
     <div className="main-container">
       <div className="digital-card">
         <Info />
         <About />
-        <Interest/>
-        <Icons/>
+        <Interest />
+        <Icons />
       </div>
     </div>
   );
